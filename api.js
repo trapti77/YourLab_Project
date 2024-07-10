b930b0c9c5d109a8c9e9dc549abea5617966b1eb
trapti patel <traptipatel46@gmail.com>
2024-07-10 22:45:23 +0530

    messagecreated

--- api.js
+++ api.js
@@ -8,12 +8,15 @@ const port = process.env.PORT || 7000;
 app.get('/about', (req, res) => {
   res.json({ name: 'trapti patel',course:'Btech CSE' });
 });
+app.get('/', (req, res) => {
+  res.json({message:'your website successfully submited',success:'true'});
+});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.get('/more', (req, res) => {
-  res.json(list_api);
+app.get('*', (req, res) => {
+  res.json({message:'404 not found ',success:'false'});
 });
 app.post('/more', (req, res) => {
   const new_list = req.body;
